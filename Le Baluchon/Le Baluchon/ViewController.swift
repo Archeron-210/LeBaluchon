af06e042f4bacb7c2a7bf78c09fae99858c3df7c
Laura Jacquin <laura.jcqn@live.fr>
2021-11-15 16:39:22 +0100

    Project creation

---
+++ Le Baluchon/Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Archeron on 15/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
