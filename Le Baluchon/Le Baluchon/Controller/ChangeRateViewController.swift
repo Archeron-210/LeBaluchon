01e100af255e9993c4f838022d550b3303b228bf
Laura Jacquin <laura.jcqn@live.fr>
2021-11-24 16:55:40 +0100

    segmented control added and configured in ChangeRate Scene / ViewController

--- Le Baluchon/Le Baluchon/Controller/ChangeRateViewController.swift
+++ Le Baluchon/Le Baluchon/Controller/ChangeRateViewController.swift
@@ -20,18 +20,19 @@ class ChangeRateViewController: UIViewController {
     @IBOutlet weak var dollarsTextField: UITextField!
     @IBOutlet weak var convertButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var currencySegmentedControl: UISegmentedControl!
 
 //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         toggleActivityIndicator(shown: false)
         makeConvertButtonCornersRounded()
+        segmentedControlColorManagement()
     }
 
 //MARK: - Functions
     @IBAction func toggleConvertButton(_ sender: UIButton) {
         updateDollarsTextField()
-        print(currentDate)
     }
 
     private func updateDollarsTextField() {
@@ -41,12 +42,13 @@ class ChangeRateViewController: UIViewController {
 
     private func convertEurtoUSD() -> String? {
 
-        guard let changeRate = currentChangeRate else {
+        guard let changeRate = currentChangeRate, changeRate.date == currentDate else {
             toggleActivityIndicator(shown: true)
             obtainCurrentChangeRate()
             return nil
         }
         guard let eurosTextFieldText = eurosTextField.text else {
+            emptyTextFieldAlert()
             return nil
         }
         guard let eurosAmount = Double(eurosTextFieldText) else {
@@ -84,9 +86,16 @@ class ChangeRateViewController: UIViewController {
         return currentDate
     }
 
-//MARK: - Alert
+//MARK: - Alerts
     private func errorAlert() {
-        let alert = UIAlertController(title: "Erreur", message: "Il semble que le courant passe mal avec le serveur...", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Erreur", message: "Il semble que le courant passe mal avec le serveur 🔌", preferredStyle: .alert)
+        let actionAlert = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(actionAlert)
+        present(alert, animated: true, completion: nil)
+    }
+
+    private func emptyTextFieldAlert() {
+        let alert = UIAlertController(title: "Erreur", message: "Il faut d'abord entrer un montant pour le convertir 💵", preferredStyle: .alert)
         let actionAlert = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         alert.addAction(actionAlert)
         present(alert, animated: true, completion: nil)
@@ -101,6 +110,11 @@ class ChangeRateViewController: UIViewController {
 
     private func makeConvertButtonCornersRounded() {
         convertButton.layer.cornerRadius = 25.0
+    }
+
+    private func segmentedControlColorManagement() {
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)]
+        currencySegmentedControl.setTitleTextAttributes(titleTextAttributes, for: .selected)
     }
 }
 
