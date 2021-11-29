d589fecce64abc1e7824e3d0845bc12690e967f7
Laura Jacquin <laura.jcqn@live.fr>
2021-11-29 15:36:35 +0100

    added Traductor.swift file with a Language enum and a mapping of api json data

---
+++ Le Baluchon/Le Baluchon/Model/Traductor.swift
@@ -0,0 +1,20 @@
+
+import Foundation
+
+enum Language {
+    case english
+    case french
+}
+
+//  Mapping JSON data from API response using Codable Protocol to parse it later  :
+struct TranslationData: Codable {
+    var data: TranslationDetails
+}
+
+struct TranslationDetails: Codable {
+    var translations: TranslatedText
+}
+
+struct TranslatedText: Codable {
+    var translatedText: String
+}
