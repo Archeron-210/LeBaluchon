25f70770d7223e6ad7b2a5b5ca98da7b54622225
Laura Jacquin <laura.jcqn@live.fr>
2021-12-01 15:28:27 +0100

    added outlets and ui aspect functions in weatherviewcontroller

--- Le Baluchon/Le Baluchon/View/WeatherViewController.swift
+++ Le Baluchon/Le Baluchon/View/WeatherViewController.swift
@@ -3,28 +3,37 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
-
+    // MARK: - Outlets
     @IBOutlet weak var nycTemperatureLabel: UILabel!
     @IBOutlet weak var nycWeatherIcon: UIImageView!
-
+    @IBOutlet weak var nycConditionLabel: UILabel!
     @IBOutlet weak var homeTemperatureLabel: UILabel!
     @IBOutlet weak var homeWeatherIcon: UIImageView!
-
+    @IBOutlet weak var homeConditionLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
-
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
-    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toggleActivityIndicator(shown: false)
+        setRefreshButtonCorners()
+    }
+
 
     @IBAction func toggleRefreshButton(_ sender: UIButton) {
     }
 
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        activityIndicator.isHidden = true
+    // MARK: - UI Aspect
+    private func toggleActivityIndicator(shown: Bool) {
+        refreshButton.isHidden = shown
+        activityIndicator.isHidden = !shown
+        shown ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
+    }
+
+    private func setRefreshButtonCorners() {
         refreshButton.layer.cornerRadius = 25.0
-
     }
 }
